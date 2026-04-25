b69824c7a0f343b0fb86508556a26e812b53acc8
TylerDuranceau <Tyler.Duranceau@smwc.edu>
2026-04-25 02:02:10 +0000

    Switch to postgres for deployment

--- config/db.js
+++ config/db.js
@@ -1,8 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite'
-});
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL, {
+      dialect: 'postgres',
+      protocol: 'postgres',
+      logging: false
+    })
+  : new Sequelize({
+      dialect: 'sqlite',
+      storage: './database.sqlite',
+      logging: false
+    });
 
 module.exports = sequelize;
